425ff0a4bb7a0423c5b2b218c8ac354b1a56a40e
anwenyao <anwenyao1997@foxmail.com>
2018-04-21 15:35:21 +0800

    分享经验模块添加了发表经验功能

--- web/js/experience.js
+++ web/js/experience.js
@@ -1,6 +1,59 @@
 window.onload =function(){
 	experience_event();
-}
+	var close = document.getElementById('close');
+	var mask = document.getElementsByClassName('mask')[0];
+	var newEx = document.getElementsByClassName('new')[0];
+	var emoijBtn = document.getElementsByClassName('emoijBtn')[0];
+	var newExp = document.getElementsByClassName('newExp')[0];
+	var emoij = document.getElementsByClassName('emoij')[0];
+	var texts = document.getElementsByClassName('text');
+	//点击发表界面
+	eventUtil.addHandler(newEx,'click',function(){
+			ifHide(mask,true);
+		});
+	//end********************************
+
+	//点击关闭界面
+	eventUtil.addHandler(close,'click',function(){
+			ifHide(mask,false);
+		});
+	//end********************************
+
+	//点击显示表情
+	eventUtil.addHandler(emoijBtn,'click',function(event){
+		event = eventUtil.getEvent(event);
+		eventUtil.stopPropagation(event);
+		ifHide(emoij,true);	
+	});
+	eventUtil.addHandler(emoij,'click',function(event){
+		event = eventUtil.getEvent(event);
+		eventUtil.stopPropagation(event);
+	});
+	//emoij隐藏
+	eventUtil.addHandler(newExp,'click',function(){
+		ifHide(emoij,false);
+		});
+	//end ******************************
+
+	//点击红框
+	highlightBorder(texts);
+	//end*********************************
+
+
+	//点击表情显示在文本框
+	var emoijs = emoij.getElementsByTagName('span');
+	var textarea = document.getElementsByClassName('textarea')[0];
+	for(i = 0;i < emoijs.length;++i){
+		eventUtil.addHandler(emoijs[i],'click',function(){
+			// console.log(textarea.value);
+			textarea.value += this.innerHTML;
+		});
+	}
+	
+	//end*******************************
+
+
+}//end of onload
 
 //experience.html
 function experience_event(){
@@ -31,6 +84,7 @@ function experience_event(){
 
 
 
+//元素是否选中
 function experience_selected(node,bool){
   if(bool){
     node.style.background = "white";
