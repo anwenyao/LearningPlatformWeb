cddf356082aea4da67b192cf02da2d61f18b91eb
anwenyao <anwenyao1997@foxmail.com>
2018-04-21 16:21:30 +0800

    完成登录的简单验证用户名和密码后跳转到个人中心

--- web/js/index.js
+++ web/js/index.js
@@ -1,36 +1,36 @@
 window.onload =function(){
-	index_event();
-}
 
-
-
-//index.html
-function index_event(){
+	//点击登录验证用户名和密码
+	var warnning = document.getElementById('warnning');
+	var signin = document.getElementById('signin');
+	var user = document.getElementById('user');
+	var psw = document.getElementById('psw');
 	var texts = document.getElementsByClassName("text");
 	var buttons = document.getElementsByClassName("button");
 	var close = document.getElementById("close");
 	var mask = document.getElementsByClassName("mask")[0];
 	var login = document.getElementById("login");
+	eventUtil.addHandler(signin,'click',function(){
+		
+		if (user.value == 'admin' && psw.value == '123') {
+			window.location = "./homepage.html";
+			console.log(1);
+			warnning.innerHTML = "";
+		}else{
+			warnning.innerHTML = "<span class='warnning cr'>!</span>用户名或者密码错误";
+		}
+	});
+
+	
 	//设置input的红边框
 	highlightBorder(texts);
 	//end********************************
 
-	//取消登录模拟button的a标签的默认行为
-	for(var i = 0;i < buttons.length;++i){
-		buttons[i].addEventListener('click',function(event){
-			if(event.preventDefault){
-            	event.preventDefault();
-            }else{
-            	event.returnValue=false;
-            }
-		});
-	}
-
-	//end********************************
 
 	//点击关闭登录界面
 	eventUtil.addHandler(close,'click',function(){
 			ifHide(mask,false);
+			warnning.innerHTML = '';
 		});
 	//end********************************
 
@@ -38,9 +38,11 @@ function index_event(){
 	//点击显示登录界面
 	eventUtil.addHandler(login,'click',function(){
 			ifHide(mask,true);
+			alert('用户名：admin，密码：123');
 		});
 	//end********************************
-}// index.html end
+}
+
 
 
 
